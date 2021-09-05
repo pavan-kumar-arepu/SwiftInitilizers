df42eba68a24df70e6fe695c3d816db06d2afe78
Pavankumar Arepu <iosdevleoper.ipa@gmail.com>
2021-09-05 11:07:42 +0530

    Added new structures and classes

--- InitiaizerEx/ViewController.swift
+++ InitiaizerEx/ViewController.swift
@@ -15,6 +15,46 @@ struct Fahrenheit {
     }
 }
 
+struct FahrenheitSmart {
+    var temperature = 32.0
+}
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+    init(white: Double) {
+        red   = white
+        green = white
+        blue  = white
+    }
+}
+
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        print(text)
+    }
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -25,8 +65,22 @@ class ViewController: UIViewController {
         print("The default temperature is \(f.temperature)° Fahrenheit")
         // Prints "The default temperature is 32.0° Fahrenheit"
 
+        let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+        // boilingPointOfWater.temperatureInCelsius is 100.0
+        let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+        // freezingPointOfWater.temperatureInCelsius is 0.0
+
+        let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+        let halfGray = Color(white: 0.5)
+        
+        let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+        cheeseQuestion.ask()
+        // Prints "Do you like cheese?"
+        cheeseQuestion.response = "Yes, I do like cheese."
+
+
+
     }
 }
-
 
 
